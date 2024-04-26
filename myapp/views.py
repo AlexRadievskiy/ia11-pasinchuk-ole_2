618e0b8b820c9f9f318f5d711d268a2bad3394d8
radie <Radievskiy2003@gmail.com>
2024-04-26 18:35:36 +0300

    Clear master

--- myapp/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Parent
-from .serializers import ParentSerializer
-
-
-class ParentViewSet(viewsets.ModelViewSet):
-    queryset = Parent.objects.all()
-    serializer_class = ParentSerializer
