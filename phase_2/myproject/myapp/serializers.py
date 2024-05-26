9e4695cef1a2904f0d26a35472dc0ced4c93e05a
radie <Radievskiy2003@gmail.com>
2024-05-26 04:30:27 +0300

    feature/phase1 done (I home)

--- phase_2/myproject/myapp/serializers.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import serializers
-from .models import Passenger
-
-
-class PassengerSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Passenger
-        fields = '__all__'
--- phase_2/myproject/myapp/views.py
+++
@@ -1,8 +0,0 @@
-from rest_framework import viewsets
-from .models import Passenger
-from .serializers import PassengerSerializer
-
-
-class PassengerViewSet(viewsets.ModelViewSet):
-    queryset = Passenger.objects.all()
-    serializer_class = PassengerSerializer
