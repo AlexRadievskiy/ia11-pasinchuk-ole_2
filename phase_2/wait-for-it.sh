c65295d31d477ccc059eb75e8f0fc0f5960916d3
radie <Radievskiy2003@gmail.com>
2024-05-26 03:34:34 +0300

    feature/phase2 second

--- phase_2/wait-for-it.sh
+++ phase_2/wait-for-it.sh
@@ -1,33 +1,21 @@
 #!/usr/bin/env bash
 #   Use this script to test if a given TCP host/port are available
 
-TIMEOUT=15
-QUIET=0
-HOST="$1"
-PORT="$2"
+set -e
+
+HOST=$1
+PORT=$2
 shift 2
+CMD="$@"
 
-while getopts "qt:" opt; do
-    case "$opt" in
-    q) QUIET=1 ;;
-    t) TIMEOUT="$OPTARG" ;;
-    esac
-done
-
-if ! command -v nc > /dev/null; then
-    echo 'Error: nc is not installed.'
-    exit 1
+if [ -z "$HOST" -o -z "$PORT" ]; then
+  echo "Usage: $0 host port [cmd...]" >&2
+  exit 1
 fi
 
-for i in $(seq $TIMEOUT); do
-    if nc -z "$HOST" "$PORT"; then
-        if [ $QUIET -eq 0 ]; then
-            echo "$HOST:$PORT is available after $i seconds."
-        fi
-        exit 0
-    fi
-    sleep 1
+while ! nc -z $HOST $PORT; do
+  echo "Waiting for $HOST:$PORT..."
+  sleep 1
 done
 
-echo "$HOST:$PORT is not available after $TIMEOUT seconds."
-exit 1
+exec $CMD
